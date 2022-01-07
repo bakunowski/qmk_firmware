2a9af3f4da0b14e00fb45d8d718e5bd08d14ca96
Karol Bakunowski <karol.bakunowski@sky.uk>
2022-01-07 23:08:44 +0000

    Test SPLIT_USB_DETECT

--- keyboards/crkbd/keymaps/via/config.h
+++ keyboards/crkbd/keymaps/via/config.h
@@ -21,6 +21,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 /* Select hand configuration */
 
 #define MASTER_LEFT
+#define SPLIT_USB_DETECT
 // #define MASTER_RIGHT
 // #define EE_HANDS
 
